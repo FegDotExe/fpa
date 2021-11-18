ea2fcc10ddc53db0341dd4904cdd9e9b378a8099
FireFlashing <filippoettoreg@gmail.com>
2021-11-18 18:21:51 +0100

    First working implementation of Pointer classes

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ class Container():
         GraphicalBase.container=self
         GraphicalBase.first=None
         self.updatedList=[]#The objects updated in an update cycle; is needed in order to not update the same object twice
+        self.windowPointedBy=[]#List of objects which point the window and will be updated on resize
     def resize(self,width:int,height:int) -> None:
         """A function which should be run when the video window is resized"""
         self.screen=pygame.display.set_mode((width,height),pygame.RESIZABLE)
         #TODO: do all the other stuff which needs to be done on resize
+        if len(self.windowPointedBy)>0:
+            GraphicalBase.first=0
+            GraphicalBase.container.updatedList=[]
+            for graphicalObject in self.windowPointedBy:
+                graphicalObject.update()
+                print(graphicalObject)
+
+            GraphicalBase.first=None
     def draw(self) -> None:
         """Draws all the objects stored in the object_dict"""
         for keyname in self.object_dict:
@@ -31,10 +40,7 @@ class GraphicalObject():
     def __init__(self,name:str,pos_pointers,size_pointers) -> None:
         GraphicalBase.container.object_dict[name]=self
         #TODO: should add a random name specifier; should prob use a static variable
-        self._x=None
-        self._y=None
-        self._xSize=None
-        self._ySize=None
+        self._name=name
         self.pointedBy=[]
         self.sizePointers=size_pointers
         self.posPointers=pos_pointers
@@ -48,13 +54,39 @@ class GraphicalObject():
             self.sizePointers[0].initialize(self)
         if self.sizePointers[1]!=None:
             self.sizePointers[1].initialize(self)
+    
+    #Properties
+    @property
+    def x(self):
+        return None
+    @x.setter
+    def x(self,value):
+        self.beginUpdate()
+    @property
+    def y(self):
+        return None
+    @y.setter
+    def y(self,value):
+        self.beginUpdate()
+    @property
+    def xSize(self):
+        return None
+    @xSize.setter
+    def xSize(self,value):
+        self.beginUpdate()
+    @property
+    def ySize(self):
+        return None
+    @ySize.setter
+    def ySize(self,value):
+        self.beginUpdate()
 
     def draw(self):
         """This is where the drawing action should be"""
         pass
 
     def beginUpdate(self):
-        print(GraphicalBase.first)
+        #If this gets changed, the logic in Container.resize() should be changed too
         if GraphicalBase.first==None:
             GraphicalBase.first=self
             GraphicalBase.container.updatedList=[]
@@ -65,16 +97,20 @@ class GraphicalObject():
         """This function is called when a graphical parameter which can have influence on other objects is called"""
         if self not in GraphicalBase.container.updatedList:
             GraphicalBase.container.updatedList.append(self)#It's important to have this at the beginning so that the class is not updated after this
+            #print(self)
             if self.posPointers[0]!=None:#x pos
                 self.x=self.posPointers[0].getValue()
             if self.posPointers[1]!=None:#y pos
                 self.y=self.posPointers[1].getValue()
             if self.sizePointers[0]!=None:#x size
-                self.ySize=self.sizePointers[0].getValue()
+                self.xSize=self.sizePointers[0].getValue()
             if self.sizePointers[1]!=None:#y size
                 self.ySize=self.sizePointers[1].getValue()
             for elements in self.pointedBy:
                 elements.update()
+    
+    def __str__(self):
+        return "<'name':'"+self._name+"', 'pos':("+str(self.x)+","+str(self.y)+"), 'size':("+str(self.xSize)+","+str(self.ySize)+")>"
 
 class GraphicalRectangle(GraphicalObject):
     def __init__(self, name: str, rect:pygame.Rect,color=(0,0,0),pos_pointers=(None,None),size_pointers=(None,None),init_update=True) -> None:
@@ -96,7 +132,7 @@ class GraphicalRectangle(GraphicalObject):
     def y(self):
         return self._rect.top
     @y.setter
-    def x(self,value):
+    def y(self,value):
         self._rect.top=value
         self.beginUpdate()
     @property
@@ -117,11 +153,21 @@ class GraphicalRectangle(GraphicalObject):
     def draw(self):
         pygame.draw.rect(surface=GraphicalBase.container.screen,color=self._color,rect=self._rect)
 
+#TODO: create a graphical object which rapresents windows, in order to be able to make updates using it
+#TODO: add layer logic to container draw
+
 class GraphicalSprite(GraphicalObject):
-    def __init__(self, name: str, image:pygame.Surface,coords=(0,0),pos_pointers=(None,None),size_pointers=(None,None),init_update=True) -> None:#TODO: add a way to handle size
+    def __init__(self, name: str, image:pygame.Surface,coords=(0,0),size=(None,None),pos_pointers=(None,None),size_pointers=(None,None),init_update=True) -> None:#TODO: add a way to handle size
         super().__init__(name,pos_pointers,size_pointers)
         self._image=image
         self._coords=coords#TODO: add a way to handle coords
+
+        if size[0]!=None:
+            self.xSize=size[0]
+            init_update=False
+        if size[1]!=None:
+            self.ySize=size[1]
+            init_update=False
 
         if init_update:
             self.beginUpdate()#TODO: see if this is actually ok
@@ -138,19 +184,22 @@ class GraphicalSprite(GraphicalObject):
         return self._coords[1]
     @y.setter
     def y(self,value):
-        print("sas")
         self._coords=(self._coords[0],value)
         self.beginUpdate()
     @property
     def xSize(self):
-        return self._image.get_width
+        return self._image.get_width()
     @xSize.setter
     def xSize(self,value):
-        self._xSize=value
-        #TODO: for object is self.sizePointed: object.update()
+        self._image=pygame.transform.scale(self._image,(value,self.ySize))
+        self.beginUpdate()
     @property
     def ySize(self):
-        return self._image.get_height
+        return self._image.get_height()
+    @ySize.setter
+    def ySize(self,value):
+        self._image=pygame.transform.scale(self._image,(self.xSize,value))
+        self.beginUpdate()
 
     def draw(self):
         GraphicalBase.container.screen.blit(self._image,self._coords)
@@ -166,14 +215,16 @@ class Pointer():
     def initialize(self,outer_object):
         pass
 
+#Static pointers
 class IntPointer(Pointer):
     def __init__(self, value:int) -> None:
         self.value=value
     def getValue(self):
         return self.value
 
+#Object pointers
 class VariablePointer(Pointer):
-    """Returns the value of a variable in a given object; before the value is returned, the object is updated"""
+    """Returns the value of a variable in a given object; before the value is returned, the object is updated; you can't make self-references, but you can find a way around the problem by modifying the same targeted value"""
     def __init__(self, object:GraphicalObject, variable:str) -> None:
         self.object=object
         self.variable=variable
@@ -182,3 +233,52 @@ class VariablePointer(Pointer):
         return getattr(self.object,self.variable)
     def initialize(self,outer_object):
         self.object.pointedBy.append(outer_object)
+#TODO: Add self-reference for variables
+#TODO: Add a better way to refer to objects
+
+class WindowPointer(Pointer):
+    """Returns the height or width of the game window"""
+    def __init__(self,varInt:int):
+        """The varInt should be 0 for width and 1 for height"""
+        self.varInt=varInt
+    def getValue(self):
+        if self.varInt==0:
+            return GraphicalBase.container.screen.get_width()
+        else:
+            return GraphicalBase.container.screen.get_height()
+    def initialize(self, outer_object):
+        GraphicalBase.container.windowPointedBy.append(outer_object)
+
+#Operational pointers
+class SumPointer(Pointer):
+    """Returns the sum of the values of the two given pointers"""
+    def __init__(self, pointer1:Pointer, pointer2:Pointer) -> None:
+        self.pointer1=pointer1
+        self.pointer2=pointer2
+    def getValue(self):
+        return self.pointer1.getValue()+self.pointer2.getValue()
+    def initialize(self, outer_object):#Initializes the inner objects
+        self.pointer1.initialize(outer_object)
+        self.pointer2.initialize(outer_object)
+
+class SubPointer(Pointer):
+    """Returns the subtraction of the values of the two given pointers"""
+    def __init__(self, pointer1:Pointer, pointer2:Pointer) -> None:
+        self.pointer1=pointer1
+        self.pointer2=pointer2
+    def getValue(self):
+        return self.pointer1.getValue()-self.pointer2.getValue()
+    def initialize(self, outer_object):
+        self.pointer1.initialize(outer_object)
+        self.pointer2.initialize(outer_object)
+
+class DivPointer(Pointer):
+    """Returns the division of the values of the two given pointers; Pointer1/Pointer2"""
+    def __init__(self, pointer1:Pointer, pointer2:Pointer) -> None:
+        self.pointer1=pointer1
+        self.pointer2=pointer2
+    def getValue(self):
+        return self.pointer1.getValue()/self.pointer2.getValue()
+    def initialize(self, outer_object):
+        self.pointer1.initialize(outer_object)
+        self.pointer2.initialize(outer_object)
